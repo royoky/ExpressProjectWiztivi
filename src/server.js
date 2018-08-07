7196a0a93db471f0bd41c370c335615d33423ec3
superadmin <ronan.marzin@gmail.com>
2018-08-07 11:33:39 +0200

    read moviesTab.json file

--- src/server.js
+++ src/server.js
@@ -1,1 +1,38 @@
-console.log('ok')
+//const express = require('express'); //old version JS
+import express from 'express'
+import fs from 'fs'
+const app = express()
+
+
+app.get('/ping', (req, res) => {
+    /*     res
+            .status(200)
+            //.setHeader('content-type', 'text/html')
+        res.send('pong') */
+    readJSONFile('public/data/moviesTab.json', (err, json) => {
+        if (err) { throw err; }
+        //console.log(json);
+        res.json(json)
+    })
+})
+/**
+ * 
+ * @param {*} filename 
+ * @param {*} callback 
+ */
+function readJSONFile(filename, callback) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            callback(err)
+            return
+        }
+        try {
+            callback(null, JSON.parse(data));
+        } catch (exception) {
+            callback(exception)
+        }
+    })
+}
+
+app.listen(5000)
+
